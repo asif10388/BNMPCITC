d1112e7be8fac13141ef16ca4a72f0622cc6612e
asif10388 <asif.khan10388@gmail.com>
2020-07-31 07:18:29 +0600

    fixed contrast issue

--- src/pages/about/about-activities/about-activities.component.jsx
+++ src/pages/about/about-activities/about-activities.component.jsx
@@ -37,7 +37,7 @@ const AboutActivities = ({ heading, subtitle }) => {
           <div class="lg:w-2/5 md:w-1/2 md:pr-10 md:py-6">
             <div class="flex relative pb-12">
               <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-                <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                <div class="h-full w-1 bg-gray-400 pointer-events-none"></div>
               </div>
               <div class="flex-shrink-0 w-10 h-10 rounded-full bg-green-400 inline-flex items-center justify-center text-white relative z-10">
                 <svg
@@ -65,7 +65,7 @@ const AboutActivities = ({ heading, subtitle }) => {
             </div>
             <div class="flex relative pb-12">
               <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-                <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                <div class="h-full w-1 bg-gray-400 pointer-events-none"></div>
               </div>
               <div class="flex-shrink-0 w-10 h-10 rounded-full bg-green-400 inline-flex items-center justify-center text-white relative z-10">
                 <svg
@@ -93,7 +93,7 @@ const AboutActivities = ({ heading, subtitle }) => {
             </div>
             <div class="flex relative pb-12">
               <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-                <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                <div class="h-full w-1 bg-gray-400 pointer-events-none"></div>
               </div>
               <div class="flex-shrink-0 w-10 h-10 rounded-full bg-green-400 inline-flex items-center justify-center text-white relative z-10">
                 <svg
@@ -121,7 +121,7 @@ const AboutActivities = ({ heading, subtitle }) => {
             </div>
             <div class="flex relative pb-12">
               <div class="h-full w-10 absolute inset-0 flex items-center justify-center">
-                <div class="h-full w-1 bg-gray-200 pointer-events-none"></div>
+                <div class="h-full w-1 bg-gray-400 pointer-events-none"></div>
               </div>
               <div class="flex-shrink-0 w-10 h-10 rounded-full bg-red-400 inline-flex items-center justify-center text-white relative z-10">
                 <svg
